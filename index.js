dd44a65d2021cf0394de9e3c87e06e8ae2d8483e
Kivelä Emil V I <emil.kivela@gmail.com>
2026-03-26 12:15:45 +0200

    Change message for test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World, Watchtower test</h1>')
+  res.send('<h1>Hello World, Watchtower test 2</h1>')
 })
 
 const PORT = 8080
